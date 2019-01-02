3791b732f770dcccb60c742ed19566b1395f3fc3
rafiswe <rafikulislam521@gmail.com>
2019-01-02 14:57:06 +0600

    new php file added

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+class ForceUser{
+
+	public function userName(){
+		return $name = "Rafikul Islam Rafi";
+	}
+	public function userDetails(){
+		$data = array(
+			'name' => $this->userName(),
+			'email' => "rafikulswe@gmail.com",
+			'mobile' => "01729346959"
+
+		);
+		return $data;
+	}
+}
+$forceUser = new ForceUser;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Object Oriented Programming in php</title>
+</head>
+<body>
+	<h4>Here we practice about oop in php.</h4>
+	<p><?php echo $forceUser->userName();?></p>
+	
+	<?php 
+		$value = $forceUser->userDetails();
+		foreach($value as $val){
+	?>
+	<h5><?= $val;?></h5>
+	<?php
+		}
+	?>
+
+
+
+	
+</body>
+</html>
